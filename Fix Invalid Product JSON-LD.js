c15cf3431953337be30f3eee2d73c3d007e6ba64
poochkingdom <webmaster@poochkingdom.com>
2024-06-17 21:04:12 -0700

    Add files via upload
    Initial commit of Fix Product JSON-LD

---
+++ Fix Invalid Product JSON-LD.js
@@ -0,0 +1,74 @@
+<!-- Fix Product JSON-LD by Alan Moore (webmaster@poochkingdom.com) -->
+<!-- This script fixes the badly formatted JSON-LD block -->
+<!-- containing the Product entry added by Squarespace -->
+<script type="text/javascript">
+  window.addEventListener('DOMContentLoaded', function () {
+    var debug = false;
+    <!-- Find all instances of JSON-LD -->
+    var jsonLdScripts = document.querySelectorAll('script[type="application/ld+json"]');
+    <!-- For each found listing -->
+    for(var jsonLdScript of jsonLdScripts) {  
+      <!-- Only process Product entries -->
+      if(jsonLdScript.innerText.indexOf('"@type":"Product"') >= 0) {
+        <!-- Parse the current JSON-LD entry -->
+        var jsonLd = JSON.parse(jsonLdScript.innerText);
+        <!-- If the Product entry has no MerchantReturnPolicy -->
+        if(!jsonLd.hasOwnProperty('hasMerchantReturnPolicy')) {
+          <!-- Provide a JSON object containing your own MerchantReturnPolicy -->
+          var returnPolicy = {"@context": "http://schema.org","@type": "MerchantReturnPolicy", "applicableCountry": "US","merchantReturnDays": 14, "returnPolicyCountry": "US", "returnPolicyCategory": "https://schema.org/MerchantReturnNotPermitted","refundType": "https://schema.org/ExchangeRefund"};
+          if(debug) console.log('Adding return policy to ' + jsonLd['name'] + ' because policy is missing');
+          <!-- Add the hasMerchantReturnPolicy -->
+          jsonLd.offers.hasMerchantReturnPolicy = returnPolicy;
+        }
+        <!-- If the Product entry has no priceValidUntil -->  
+        if(!jsonLd.hasOwnProperty('priceValidUntil')) {
+          <!-- Set the priceValidUntil date to midnight on Dec 31 in the current year PST -->
+          var priceValidUntil = new Date().getFullYear() + '-12-31T23:59:59-08:00';
+          if(debug) console.log('Adding priceValidUntil to ' + jsonLd['name'] + ' because it is missing');
+          <!-- Add the priceValidUntil -->
+          jsonLd.offers.priceValidUntil = priceValidUntil;
+        }
+        <!-- If the Product entry has no brand type -->
+        if(!jsonLd.brand.hasOwnProperty('@type')) {
+          var brand = {"@type": "Brand","name": "Pooch Kingdom Luxury Dog Bakery"};
+          if(debug) console.log('Adding brand name to ' + jsonLd['name'] + ' because it is invalid');
+          <!-- Add the Brand -->
+          jsonLd.brand = brand;
+        }
+        <!-- If everything is fixed, delete the original entry and insert a new product JSON-LD -->
+        if(jsonLd.offers.hasOwnProperty('hasMerchantReturnPolicy') && jsonLd.offers.hasOwnProperty('priceValidUntil') && jsonLd.brand.hasOwnProperty('@type')) {
+          <!-- Convert the object back into a string -->
+          <!-- And update the current JSON-LD entry -->
+          jsonLdScript.innerText = JSON.stringify(jsonLd);
+          <!-- Get the finalized version of the Product -->
+          var newProductJsonLd = '<!-- Injected By Fix Product JSON-LD In Store | Page Header Code Injection --><script type="application/ld+json">';
+          newProductJsonLd += jsonLdScript.innerText;
+          newProductJsonLd += '<\/script><!-- End Injected By Fix Product JSON-LD -->';
+          if(debug) console.log(newProductJsonLd);
+          <!-- Remove the old version from the document -->
+          jsonLdScript.remove();
+          <!-- Insert a new Product based on the modified version -->
+          document.head.insertAdjacentHTML('beforeEnd', newProductJsonLd);
+        }
+        break;
+      }
+    }
+  });
+</script>
+<!-- End Fix Product JSON-LD -->
+
+
+/* You can only add non-zero ratings or review counts    
+      <!-- If the Product entry has no aggregateRating -->  
+      if(!jsonLd.hasOwnProperty('aggregateRating')) {
+        <!-- Provide a JSON object containing your own MerchantReturnPolicy -->
+        var aggregateRating = {"@type": "AggregateRating","ratingValue": "0.0","reviewCount": "0"};
+        console.log('Adding aggregateRating to ' + jsonLd['name'] + ' because it is missing');
+        <!-- Add the aggregateRating -->
+        jsonLd.aggregateRating = aggregateRating;
+        <!-- Convert the object back into a string -->    
+        newJson = JSON.stringify(jsonLd);
+        <!-- Update the current JSON-LD entry -->    
+        jsonLdScript.innerHTML = newJson;
+      }
+*/  
